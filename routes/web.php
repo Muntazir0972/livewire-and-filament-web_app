36be2b80acea2870e95731e36eb577ec1800aaba
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-07-19 15:30:46 +0500

    configure home page,3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
-<?php
-
+ <?php
 use Illuminate\Support\Facades\Route;
+use App\Livewire\ShowHome;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', ShowHome::class)->name('home');
