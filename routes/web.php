de520e277253ba74ea1268037731958f820ea97d
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-07-24 15:25:58 +0500

    our team page,7

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\ShowHome;
 use App\Livewire\ShowService;
 use App\Livewire\ShowServicePage;
+use App\Livewire\ShowTeamPage;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -12,3 +13,4 @@ use App\Livewire\ShowServicePage;
 Route::get('/', ShowHome::class)->name('home');
 Route::get('/services', ShowServicePage::class)->name('servicesPage');
 Route::get('/service/{id}', ShowService::class)->name('servicePage');
+Route::get('/team', ShowTeamPage::class)->name('teamPage');
